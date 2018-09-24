c16329f52f65552f4c50f72440a901755a5d2486
Van <vanessa.cassandra.s@gmail.com>
2018-09-24 23:27:52 +0800

    Use data and created instead of computed

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,5 +1,5 @@
 <template>
-	<Description :title="title" description="Zero Waste is a concept that challenges the old way of thinking about waste as something that has no value and to be thrown away." :imgSrc="require('../assets/06_copositive.png')">
+	<Description :title="title" :description="description" :imgSrc="require('../assets/06_copositive.png')">
 </Description>
   </template>
 
@@ -11,15 +11,24 @@ export default{
 	components:{
 		Description
 	},
-  computed: {
-    title () {
-      let pathItem = this.$route.params.category;
-      let arrayList = this.$store.state.categories;
-      let item = arrayList.find(function(element){
-        return element.slug === pathItem;
-      })
-      return item.title;
+  data: function(){
+    return{
+      title: '',
+      description: '',
+      imgSrc: ''
     }
+    
+  },
+  created: function(){
+    let pathItem = this.$route.params.category;   //get url from router
+    let arrayList = this.$store.state.categories; //get data from store
+    let item = arrayList.find(function(element){
+      return element.slug === pathItem;
+    });
+
+    this.title = item.title;
+    this.description = item.description;
+
   }
 
 }
