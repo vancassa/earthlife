8552b8bf6f4325bd30e83e998caad0d7db6e0258
Van <vanessa.cassandra.s@gmail.com>
2018-09-24 23:28:43 +0800

    Add more complete dummy data

--- src/store.js
+++ src/store.js
@@ -8,14 +8,17 @@ export default new Vuex.Store({
     categories: [
       {
         slug: 'plantbased',
-        title: "Plant-based Diet Test",
-        description: "Anything....."
+        title: "Plant-based Diet",
+        description: "Plant-based diet is a way of eating that celebrates plant foods and cuts out unhealthy items like added sugars and refined grains.",
+        imgSrc: "../assets/01_plantbased.png"
       },
       {
         slug: 'zerowaste',
         title: "Zero Waste",
-        description: "Anything2....."
+        description: "Zero Waste is a concept that challenges the old way of thinking about waste as something that has no value and to be thrown away.",
+        imgSrc: "../assets/02_zerowaste.png"
       }
+
   ]
 
   },
