bf71756ff2a3fabd40e629a224c260d604f7af0f
Brianna-Degaston <briannadeg@gmail.com>
2018-10-01 18:20:53 +0800

    Added the Habit Image Tracker at the bottom with the buttons with each image.

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,66 +1,77 @@
 <template>
-<div class="question-wrapper">
-  <div class="question">
-  
-    <div class="question-category">{{ question.title }}</div>
-    <div class="question-text">{{ question.questionText }}</div>
-    <ul>
-      <li v-for="answer in question.answers"  class="question-answer">
-        <span>{{ answer.text }}</span>
-      </li>
-    </ul>
-    <button class='button-disabled'><v-icon name="arrow-right"/></button>
+  <div class="question-wrapper">
+    <div class="question">
 
+      <div class="question-category">{{ question.title }}</div>
+      <div class="question-text">{{ question.questionText }}</div>
+      <ul>
+        <li v-for="answer in question.answers"  class="question-answer">
+          <span>{{ answer.text }}</span>
+        </li>
+      </ul>
+      <button class='button-disabled'><v-icon name="arrow-right"/></button>
+    </div>
+    <div class="habit-tracker">
+      <img class="habit-category" src="../assets/images/buttons/plant-based.png">
+      <img class="habit-category" src="../assets/images/buttons/zero-waste.png">
+      <img class="habit-category" src="../assets/images/buttons/advocacy.png">
+      <img class="habit-category" src="../assets/images/buttons/co2.png">
+      <img class="habit-category" src="../assets/images/buttons/minimalism.png">
+      <img class="habit-category" src="../assets/images/buttons/biophilia.png">
+    </div>
   </div>
-  </div>
-  </template>
+</template>
 
 <script>
 
-export default{
-	name: 'Question',
-	components: {
+  export default{
+   name: 'Question',
+   components: {},
+   data: function(){
+    return {
+     selected: 0,
+     completed: ['plant-based', 'co2']
+   }
+ },
 
-	},
-  data: function(){
-		return{
-			selected: 0
-		}
-	},
+ computed: {
+  question: function() {
+    let categorySlug = this.$route.params.category;
+    let questionID = this.$route.params.id;
+    let categories = this.$store.state.categories; 
 
-  computed: {
-    question: function() {
-      let categorySlug = this.$route.params.category;
-      let questionID = this.$route.params.id;
-      let categories = this.$store.state.categories; 
-
-      let category = categories.find(function(c){
-        return c.slug === categorySlug;
+    let category = categories.find(function(c){
+      return c.slug === categorySlug;
       
-      });
+    });
 
-      let categoryQuestion = category.questions[questionID-1];
-      let categoryAnswers = category.questions[questionID-1].answers;
+    let categoryQuestion = category.questions[questionID-1];
+    let categoryAnswers = category.questions[questionID-1].answers;
 
-      return {
-        title: category.title,
-        questionText: categoryQuestion.text,
-        answers: categoryAnswers
-      };
-    }
+    return {
+      title: category.title,
+      questionText: categoryQuestion.text,
+      answers: categoryAnswers
+    };
   },
 
-  created: function(){
-    this.getData();
-  },
+  habitTracker: function() {
 
-
-  watch:{
-    '$route'(to, from){   //watch url change
-       this.getData();
-    }
-    
   }
+
+},
+
+created: function(){
+  this.getData();
+},
+
+
+watch:{
+    '$route'(to, from){   //watch url change
+     this.getData();
+   }
+
+ }
 }
 
 </script>
@@ -68,10 +79,10 @@ export default{
 <style>
 
 .question {
-	max-width: 960px;
-	padding: 0 36px;
-	margin: 0 auto;
-	margin-top: 218px;
+  max-width: 960px;
+  padding: 0 36px;
+  margin: 0 auto;
+  margin-top: 218px;
 }
 
 .question-wrapper {
@@ -81,52 +92,64 @@ export default{
 }
 
 .question-category {
-	color: #919EAB;
-	font-size: 12px;
-	letter-spacing: 1.4px;
-	line-height: 30px;
+  color: #919EAB;
+  font-size: 12px;
+  letter-spacing: 1.4px;
+  line-height: 30px;
 }
 
 .question-text {
-	max-width: 554px;
-	color: #212B36;
-	font-size: 24px;
-	font-weight: 500;
-	line-height: 35px;
+  max-width: 554px;
+  color: #212B36;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 35px;
 
 }
 
 .question-answer {
-	margin-top: 20px;
-	padding: 24px;
-	max-width: 560px;
-	color: #212B36;
-	font-size: 16px;
-	line-height: 22px;
-	background-color: white;
+  margin-top: 20px;
+  padding: 24px;
+  max-width: 560px;
+  color: #212B36;
+  font-size: 16px;
+  line-height: 22px;
+  background-color: white;
 }
 
 .question-answer:hover{
-	cursor: pointer;
+  cursor: pointer;
 }
 
 .button-enabled{
-	height: 52px;
-	width: 52px;
-	background-color: #4E4D86;
-	color: white;
-	border-radius: 10000px;
-	margin-top: 20px;
+  height: 52px;
+  width: 52px;
+  background-color: #4E4D86;
+  color: white;
+  border-radius: 10000px;
+  margin-top: 20px;
 }
 
 .button-disabled {
-	height: 52px;
-	width: 52px;
-	opacity: 0.5;
-	background-color: #4E4D86;
-	color: white;
-	border-radius: 10000px;
-	margin-top: 20px;
+  height: 52px;
+  width: 52px;
+  opacity: 0.5;
+  background-color: #4E4D86;
+  color: white;
+  border-radius: 10000px;
+  margin-top: 20px;
+}
+
+.habit-category {
+  width: 50px;
+  height: auto;
+  filter: grayscale(100%);
+}
+
+.habit-category.finished {
+  width: 50px;
+  height: auto;
+  filter: grayscale(0%);
 }
 
 </style>
