7837ef1032947ee4aecf921ff9d1f6458d348eb8
Van <vanessa.cassandra.s@gmail.com>
2018-10-06 16:57:34 +0800

    Change Category page to read data correctly from the store

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="category">
-    <Description v-if="startQuiz === false" :title="title" :description="description" :imgSrc="imgSrc" @startQuiz="onStartQuiz">
+    <Description :title="test.title" :description="test.description" :imgSrc="test.imgSrc">
     </Description>
-    
-    <QuizSingleChoice v-if="startQuiz === true" :category="title" :question="question" :choices="choices">
-    </QuizSingleChoice>
-
-    <div>
-        {{ test.content }}
-      </div>
   </div>
   </template>
 
@@ -23,37 +16,56 @@ export default{
     QuizSingleChoice
 	},
 
-  data: function(){
-    return{
-      title: '',
-      description: '',
-      imgSrc: '',
-      startQuiz: false,
-      question: 'When eating out, I usually...',
-      choices: [
-      {
-        index: 1,
-        content: 'Choose a restaurant that doesn\'t use disposables or bring my own reusable servingware and cutlery.',
-        score: 10
-      },
-      {
-        index: 2,
-        content: 'Don\'t really think about the waste created from eating there.',
-        score: 20
-      }
-      ]
-    }
-    
-  },
+  computed: {
 
-  computed:{
-      test: function(){
+    test: function(){
+      if(this.$store.state.categories.length > 0){
+
+        let pathItem = this.$route.params.category;   //get url from router
+        let arrayList = this.$store.state.categories; //get data from store
+        let item = arrayList.find(function(element){
+          return element.slug === pathItem;
+        });
 
         return{
-          content: this.$store.state.categories[0].title 
+          title: item.title,
+          description: item.description, 
+          imgSrc: item.imgSrc
         }
       }
-    },
+      else{
+        return{
+          title: '',
+          description: '', 
+          imgSrc: ''
+        }
+      }
+      
+    }
+
+
+    
+    // return{
+    //   title: item.title,
+    //   description: '',
+    //   imgSrc: '',
+    //   startQuiz: false,
+    //   question: 'When eating out, I usually...',
+    //   choices: [
+    //   {
+    //     index: 1,
+    //     content: 'Choose a restaurant that doesn\'t use disposables or bring my own reusable servingware and cutlery.',
+    //     score: 10
+    //   },
+    //   {
+    //     index: 2,
+    //     content: 'Don\'t really think about the waste created from eating there.',
+    //     score: 20
+    //   }
+    //   ]
+    // }
+    
+  },
 
   methods: {
     getData: function(){
@@ -63,9 +75,10 @@ export default{
         return element.slug === pathItem;
       });
 
-      this.title = item.title;
-      this.description = item.description;
-      this.imgSrc = item.imgSrc;
+      // this.title = item.title;
+      // this.description = item.description;
+      // this.imgSrc = item.imgSrc;
+      return {title: item.title, description: item.description, imgSrc: item.imgSrc}
     },
 
     onStartQuiz: function(){
@@ -74,7 +87,7 @@ export default{
   },
 
   created: function(){
-    this.getData();
+    // this.getData();
   },
 
   watch:{
