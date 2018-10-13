9fe887306cad68d091adb5cb562ab410799c49a8
Van <vanessa.cassandra.s@gmail.com>
2018-10-13 17:22:02 +0800

    Add multiple choice vue component

---
+++ src/components/QuizMultiChoice.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="quiz-multiple">
+		<div v-for="answer in choices">
+			<input type="checkbox" :id="answer.text" name="answer"
+				:value="answer.text" v-model="inputSelected" >
+			</input>
+			<label class="question-answer" :for="answer.text" :class="{'answer-selected': inputSelected.includes(answer.text)}">{{ answer.text }}</label>
+   	</div>
+	</div>     
+</template>
+
+<script>
+export default{
+	name:'QuizMultiChoice',
+	props:{
+		// category: String,
+		// question: String,
+		choices: Array
+	},
+
+	data: function(){
+    return {
+     inputSelected: []
+   }
+
+	}
+}
+</script>
+
+<style>
+input[type="checkbox"]{
+	display:none;
+}
+</style>
---
+++ src/components/QuizSlider.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="quiz-slider">
+		<div v-for="answer in choices">
+			<input type="radio" :id="answer.text" name="answer"
+				:value="answer.text" v-model="inputSelected" >
+			</input>
+			<label class="question-answer" :for="answer.text" :class="{ 'answer-selected': inputSelected == answer.text}">{{ answer.text }}</label>
+   	</div>
+	</div>     
+</template>
+
+<script>
+export default{
+	name:'QuizSlider',
+	props:{
+		// category: String,
+		// question: String,
+		choices: Array
+	},
+	data: function(){
+    return {
+     inputSelected: ""
+   }
+	}
+}
+</script>
