9a7f969a3ff10f666469a3c6ba32db2a11f78e01
Brianna-Degaston <briannadeg@gmail.com>
2018-10-20 17:36:06 +0800

    Centered text, redid some CSS styling, resized images, and nested tags

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -2,23 +2,37 @@
   <div id="result">
       <div class="global-earthlife-nav">EARTHLIFE</div>
       <div class="result-header">Your Result</div>
-      <img class="result-image" src="../assets/images/result/01_result_plantbased.jpg" alt="image goes here"/>
-      <div class="download-bttn">download</div>
-      <div class="fb-bttn">facebook</div>
+      <img class="result-image" src="../assets/images/result/01_result_plantbased.jpg"/>
+      <div class="bttns">
+          <div class="download-bttn"><a>download</a></div>
+          <div class="fb-bttn"><a>facebook</a></div>
+      </div>
       <div class="result-text">Here's how you did</div>
       <div class="habits-bar">
-          <img src="../assets/images/buttons/plant-based.png"/>
+          <div class="habit-wrapper">
+              <img class="habit-img" src="../assets/images/buttons/plant-based.png"/>
           <div class="habit-name">Plant Based</div>
-          <img src="../assets/images/buttons/zero-waste.png"/>
+          </div>
+          <div class="habit-wrapper">
+              <img class="habit-img" src="../assets/images/buttons/zero-waste.png"/>
           <div class="habit-name">Zero Waste</div>
-          <img src="../assets/images/buttons/biophilia.png"/>
+          </div>
+          <div class="habit-wrapper">
+              <img class="habit-img" src="../assets/images/buttons/biophilia.png"/>
           <div class="habit-name">Biophilia</div>
-          <img src="../assets/images/buttons/advocacy.png"/>
+          </div>
+          <div class="habit-wrapper">
+              <img class="habit-img" src="../assets/images/buttons/advocacy.png"/>
           <div class="habit-name">Advocacy</div>
-          <img src="../assets/images/buttons/minimalism.png"/>
+          </div>
+          <div class="habit-wrapper">
+              <img class="habit-img" src="../assets/images/buttons/minimalism.png"/>
           <div class="habit-name">Minimalism</div>
-          <img src="../assets/images/buttons/co2.png"/>
+          </div>
+          <div class="habit-wrapper">
+              <img class="habit-img" src="../assets/images/buttons/co2.png"/>
           <div class="habit-name">CO2 Positive</div>
+          </div>  
       </div>
       <div class="graph">Graph goes here</div>
       <div class="step2">
@@ -48,9 +62,60 @@ export default{
 }
 
 .result-image {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 45%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 45%;
+}
+
+.bttns {
+    text-align: center;
+}
+
+.download-bttn {
+  margin: 10px;
+  display: inline-block;
+}
+
+.fb-bttn {
+  margin: 10px;
+  display: inline-block;
+}
+
+.result-text {
+  color: #403E3D; 
+  font-family: Poppins;   
+  font-size: 24px;    
+  font-weight: 500;   
+  line-height: 35px;  
+  text-align: center;
+}
+
+.habits-bar {
+
+}
+
+.habit-wrapper {
+    display: inline-block;
+}
+
+.habit-name {
+}
+
+.habit-img {
+    height: auto;
+    width: 25%;
+}
+
+.graph {
+
+}
+
+.step2 {
+
+}
+
+.get-actions-bttn {
+
 }
 </style>
