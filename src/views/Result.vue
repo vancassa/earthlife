d95f778b2aa5480f29aa80ffa973fb6bcde11131
Brianna-Degaston <briannadeg@gmail.com>
2018-10-20 15:41:49 +0800

    Fixed white spaces

---
+++ src/views/Result.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="result"></div>
+</template>
+
+<script>
+export default{
+  name:'Result',
+  components: {
+
+  },
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Poppins', sans-serif;
+  text-align: left;
+  color: #2c3e50;
+  height: 100%;
+}
+
+.global-earthlife-nav {
+  padding: 25px 15%;
+  color: #212B36;
+  font-size: 16px;  
+  letter-spacing: 3px;  
+  line-height: 25px;
+  -webkit-box-shadow: 0 1px 2px 0 rgba(0,0,0,0.05);
+}
+
+html{
+	height: 100%;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+}
+
+button:focus {
+  outline: 0;
+}
+
+</style>
