583c1b84d1f211dbad87a8c3444f2b02d811b3eb
Vanessa Cassandra <v.cassandra@beckhoff.com>
2018-10-26 20:34:11 +0800

    Remove whitespaces

--- src/components/QuizMultiChoice.vue
+++ src/components/QuizMultiChoice.vue
@@ -1,46 +1,46 @@
 <template>
-	<div class="quiz-multiple">
-		<div v-for="answer in choices">
-			<input type="checkbox" :id="answer.text" name="answer"
-				:value="answer.text" v-model="inputSelected" >
-			</input>
-			<label class="question-answer" :for="answer.text" :class="{'answer-selected': inputSelected.includes(answer.text)}">{{ answer.text }}</label>
-   	</div>
-	</div>     
+  <div class="quiz-multiple">
+    <div v-for="answer in choices">
+      <input type="checkbox" :id="answer.text" name="answer"
+        :value="answer.text" v-model="inputSelected" >
+      </input>
+      <label class="question-answer" :for="answer.text" :class="{'answer-selected': inputSelected.includes(answer.text)}">{{ answer.text }}</label>
+    </div>
+  </div>     
 </template>
 
 <script>
 export default{
-	name:'QuizMultiChoice',
-	props:{
-		// category: String,
-		// question: String,
-		choices: Array
-	},
+  name:'QuizMultiChoice',
+  props:{
+    // category: String,
+    // question: String,
+    choices: Array
+  },
 
-	data: function(){
+  data: function(){
     return {
      inputSelected: []
    }
 
-	},
-	
-	watch: {
-		inputSelected: function(newAnswer) {
-			var answers = [];
-			newAnswer.forEach(a => {
-				var index = this.choices.findIndex(x => x.text == a);
-				answers.push(index);
-			})
-			
-			this.$emit('answering', answers);
-		}
-	}
+  },
+  
+  watch: {
+    inputSelected: function(newAnswer) {
+      var answers = [];
+      newAnswer.forEach(a => {
+        var index = this.choices.findIndex(x => x.text == a);
+        answers.push(index);
+      })
+      
+      this.$emit('answering', answers);
+    }
+  }
 }
 </script>
 
 <style>
 input[type="checkbox"]{
-	display:none;
+  display:none;
 }
 </style>
