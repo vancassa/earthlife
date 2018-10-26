b49e6f934a03409239a15a18c51ca7a83bf263b5
Brianna-Degaston <briannadeg@gmail.com>
2018-10-26 17:51:18 +0800

    updated image in photoshop and reverted to old changes

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="question-wrapper">
-    <div class="bg-img"><img src="../assets/other-3.png" /></div>
     <div class="question">
       <div class="question-category">{{ question.title }}</div>
       <div class="question-text">{{ question.questionText }}</div>
@@ -132,23 +131,16 @@ watch:{
   max-width: 960px;
   padding: 0 36px;
   margin: 0 auto;
+  margin-top: 218px;
 }
 
 .question-wrapper {
-  background-color: #DFE3E8; 
-  
-  padding-bottom: 200px;
-}
-
-.bg-img {
+  background-image: url(../assets/other-3.png);
   background-repeat: no-repeat;
   background-position: right top;
+  background-color: #DFE3E8; 
   overflow: hidden;
-  -moz-transform: scaleX(-1);
-  -o-transform: scaleX(-1);
-  transform: scaleX(-1);
-  filter: FlipH;
-  -ms-filter: "FlipH";
+  padding-bottom: 200px;
 }
 
 .question-category {
