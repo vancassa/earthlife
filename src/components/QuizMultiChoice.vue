e277ea01195ef5e27419ef8fdf8c3fe5a7eb328e
Van <vanessa.cassandra.s@gmail.com>
2018-10-27 12:56:28 +0800

    Use self closing tag for input

--- src/components/QuizMultiChoice.vue
+++ src/components/QuizMultiChoice.vue
@@ -2,8 +2,7 @@
   <div class="quiz-multiple">
     <div v-for="answer in choices">
       <input type="checkbox" :id="answer.text" name="answer"
-        :value="answer.text" v-model="inputSelected" >
-      </input>
+        :value="answer.text" v-model="inputSelected" \>
       <label class="question-answer" :for="answer.text" :class="{'answer-selected': inputSelected.includes(answer.text)}">{{ answer.text }}</label>
     </div>
   </div>     
