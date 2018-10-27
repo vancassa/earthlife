d34cccee06e877c7ee214266cb788a10330804be
alice <alesialim@gmail.com>
2018-10-27 15:35:28 +0800

    Create progressBar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="progress-bar-wrapper">
+    <div class="progress-bar" v-bind:style="{width: progress + '%'}">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PogressBar',
+  props: {
+    progress: {
+      type: Number,
+      default: 0
+    }
+  },
+  components: {
+  },
+}
+</script>
+
+<style scoped>
+.progress-bar {
+  height: 6px;
+  background-color: #4E4D86;
+}
+</style>
