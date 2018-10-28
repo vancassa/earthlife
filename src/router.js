8084e4a52e05eb4275eb6c4bcf08f174fac41875
Brianna-Degaston <briannadeg@gmail.com>
2018-10-28 15:28:19 +0800

    removed results path in router and only kept result

--- src/router.js
+++ src/router.js
@@ -75,14 +75,5 @@ export default new Router({
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/Actions.vue")
     },
-
-      path: "/results",
-      name: "results",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Actions.vue")
-    }
   ]
 });
