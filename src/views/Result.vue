137fc6866d3fc2b327bc43ad50fcc42cd17ddffd
Brianna-Degaston <briannadeg@gmail.com>
2018-10-28 20:05:37 +0800

    offset footer

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -86,6 +86,7 @@ export default{
 .not-fixed {
   position: absolute;
   width: 100%;
+  padding-bottom: 100px;
 }
 .result-header {
   color: #403E3D; 
@@ -164,7 +165,7 @@ export default{
 
 .graph-inner {
   position: absolute;
-  top: 50%;
+  top: 30%;
   width: 100%;
 }
 
