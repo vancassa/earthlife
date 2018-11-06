5c330d4e170e8c5860974dad6716ff0b0e478e8b
Brianna-Degaston <briannadeg@gmail.com>
2018-11-07 07:44:29 +0800

    added to the router

--- src/router.js
+++ src/router.js
@@ -75,5 +75,15 @@ export default new Router({
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/Actions.vue")
     },
+     {
+      path: '/todo',
+      name: 'todo',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/ToDo.vue")
+
+    },
   ]
 });
