0177edcbd48e8ec847897bf1f0451bf5841dc697
Brianna-Degaston <briannadeg@gmail.com>
2018-11-07 07:40:38 +0800

    made a new file

---
+++ src/views/ToDo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="todo">
+   
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'Todo',
+    components: {
+
+    },
+  }
+</script>
+
+<style>
+
+
+@media (max-width: 500px) {
+  
+}
+</style>
