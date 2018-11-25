473ada9fb71ef7924831230497b067ed15195d40
Van <vanessa.cassandra.s@gmail.com>
2018-11-25 11:49:53 +0800

    Change copy icon to print icon

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import 'vue-awesome/icons/download';
 import 'vue-awesome/icons/exclamation-circle';
 import 'vue-awesome/icons/envelope';
 import 'vue-awesome/icons/copy';
+import 'vue-awesome/icons/print';
 import 'vue-awesome/icons/check-circle';
 import 'vue-awesome/icons/brands/facebook-f';
 //import 'vue-awesome/icons' UNCOMMENT IF NEEDED
